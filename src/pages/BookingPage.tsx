d5a5b3929f0dfff2956cb7b49bec67f4e878e316
Mykola Romaniv <mykolaromaniw@gmail.com>
2026-09-06 08:43:39 +0300

    Migrate to React Router v7

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo, ReactNode } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router';
 import axios from 'axios';
 import {
   CheckCircle, ChevronLeft, ChevronRight,
--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router';
 import { useAuth } from '../context/AuthContext';
 import { useLocale } from '../i18n/LocaleContext';
 import LanguageToggle from '../components/LanguageToggle';
